8d0adfeef1b70fb6081ee5d5dbfac318de11ddba
Corentin Bruneau <ccbbruneau@gmail.com>
2018-03-06 16:00:07 +0100

    First working 'dirty' version

--- gophercise-1/cmd/main.go
+++ gophercise-1/cmd/main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type question struct {
+	text   string
+	answer string
+}
+
+var (
+	questions []*question
+)
 
 func main() {
-	fmt.Println("Hello, world!")	
+	readQuestions()
+
+	total := len(questions)
+	good := 0
+
+	input := bufio.NewReader(os.Stdin)
+
+	printSeparator()
+
+	for _, q := range questions {
+		fmt.Printf("%s=", q.text)
+		attempt, _ := input.ReadString('\n')
+
+		if checkAnswer(q, strings.Trim(attempt, "\n")) {
+			good++
+		}
+	}
+
+	printSeparator()
+	fmt.Printf("You scored %d out of %d\n", good, total)
+	printSeparator()
+}
+
+func printSeparator() {
+	fmt.Println("---")
+}
+
+func checkAnswer(q *question, attempt string) bool {
+	return strings.Compare(q.answer, attempt) == 0
+}
+
+func readQuestions() {
+	rawData, err := ioutil.ReadFile("./questions.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	csvReader := csv.NewReader(bytes.NewReader(rawData))
+
+	for {
+		row, err := csvReader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		questions = append(questions, &question{
+			text:   row[0],
+			answer: row[1],
+		})
+	}
 }
