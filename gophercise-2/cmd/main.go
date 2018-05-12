5783b5e7d1ce9d087d58a099473a241d737a84cc
Corentin Bruneau <ccbbruneau@gmail.com>
2018-05-12 02:46:58 +0200

    Add json handling

--- gophercise-2/cmd/main.go
+++ gophercise-2/cmd/main.go
@@ -1,38 +1,68 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
+type urlMap map[string]string
+
 type ShortenHandler struct {
-	urlMap map[string]string
+	mapping urlMap
 }
 
 func main() {
-	http.ListenAndServe(":5050", &ShortenHandler{
-		urlMap: readMapping(),
-	})
+	var src string
+	var jsonSrc bool
+
+	flag.StringVar(&src, "map-src", "./mapping.json", "Path of the file containing the mappings")
+	flag.BoolVar(&jsonSrc, "json", false, "Indicates whether the source file is a json file")
+
+	closeChan := make(chan interface{}, 1)
+
+	go func(um urlMap) {
+		http.ListenAndServe(":5050", &ShortenHandler{
+			mapping: um,
+		})
+
+		closeChan <- nil
+	}(readMapping(src, jsonSrc))
+
+	<-closeChan
 }
 
 func (s *ShortenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := r.RequestURI[1:]
-	fmt.Println(path)
-
-	url, ok := s.urlMap[path]
+	url, ok := s.mapping[path]
 
 	if ok {
 		http.Redirect(w, r, url, http.StatusFound)
 		return
 	}
 
-	fmt.Fprintln(w, "Path '"+path+"' not mapped !")
+	fmt.Fprintf(w, "Path %q not mapped !\n", path)
 }
 
-func readMapping() map[string]string {
-	return map[string]string{
-		"test": "http://localhost:5050/popo",
-		"popo": "http://localhost:5050/toto",
-		"toto": "http://localhost:5050/end",
+func readMapping(src string, isJson bool) urlMap {
+	fmt.Println("src="+src, "isJson="+strconv.FormatBool(isJson))
+
+	data, err := ioutil.ReadFile(src)
+
+	if err != nil {
+		panic(err)
 	}
+
+	var parsedResult map[string]string
+
+	err = json.Unmarshal(data, &parsedResult)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return parsedResult
 }
