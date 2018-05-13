d6851a311baf1c8cd0bb5e85d76f8231c650480a
Corentin Bruneau <ccbbruneau@gmail.com>
2018-05-14 00:33:58 +0200

    Implement working solution

--- ex09_card_deck/cmd/main.go
+++ ex09_card_deck/cmd/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/dancing-koala/gophercises-impl/ex09_card_deck/pkg/deck"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	cfg := deck.NewConfig()
+
+	cfg.Shuffle = false
+	cfg.JokerCount = 3
+	cfg.ExtraDecks = 2
+	cfg.Filter = func(f, n uint8) bool {
+		return f >= 3
+	}
+
+	cfg.Sort = func(left, right deck.Card) bool {
+		if left.Family > right.Family {
+			return true
+		}
+
+		return left.Number <= right.Number
+	}
+
+	cardDeck := deck.New(cfg)
+
+	fmt.Println(cardDeck)
 }
---
+++ ex09_card_deck/pkg/deck/card.go
@@ -0,0 +1,115 @@
+package deck
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+const (
+	SPADE uint8 = iota
+	DIAMOND
+	CLUB
+	HEART
+	JOKER
+)
+
+type Card struct {
+	Family uint8
+	Number uint8
+}
+
+func (c *Card) Symbol() string {
+	switch c.Number {
+	case 0:
+		return "Jkr"
+	case 1:
+		return "A"
+	case 11:
+		return "J"
+	case 12:
+		return "Q"
+	case 13:
+		return "K"
+	default:
+		return fmt.Sprintf("%d", c.Number)
+	}
+}
+
+type Config struct {
+	Sort       func(left, right Card) bool
+	Shuffle    bool
+	JokerCount uint8
+	Filter     func(f, n uint8) bool
+	ExtraDecks uint8
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Shuffle:    false,
+		JokerCount: 0,
+		Filter:     defaultFilter,
+		ExtraDecks: 0,
+	}
+}
+
+func New(cfg *Config) []Card {
+
+	families := []uint8{SPADE, DIAMOND, CLUB, HEART}
+
+	decksCount := 1 + cfg.ExtraDecks
+
+	deck := make([]Card, 0, 52*decksCount+cfg.JokerCount)
+
+	for count := 0; count < int(decksCount); count++ {
+		for i := 0; i < 13; i++ {
+			for j := 0; j < 4; j++ {
+
+				if !cfg.Filter(families[j], uint8(i+1)) {
+					continue
+				}
+
+				deck = append(deck, Card{
+					Family: families[j],
+					Number: uint8(i + 1),
+				})
+			}
+		}
+	}
+
+	if cfg.JokerCount > 0 {
+		for i := 0; i < int(cfg.JokerCount); i++ {
+			deck = append(deck, Card{
+				Family: JOKER,
+				Number: 0,
+			})
+		}
+	}
+
+	if cfg.Sort != nil {
+		sort.Slice(deck, func(i, j int) bool {
+			return cfg.Sort(deck[i], deck[j])
+		})
+	}
+
+	if cfg.Shuffle {
+		shuffle(deck)
+	}
+
+	return deck
+}
+
+func defaultFilter(f, n uint8) bool {
+	return true
+}
+
+func shuffle(deck []Card) {
+	length := len(deck)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := length - 1; i >= 0; i-- {
+		j := rand.Intn(length)
+		deck[i], deck[j] = deck[j], deck[i]
+	}
+}
