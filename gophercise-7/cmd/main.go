5c5aa1fbbd30c10b6269097ac4899d260aa6137a
Corentin Bruneau <ccbbruneau@gmail.com>
2018-05-13 18:55:09 +0200

    Refactor solution

--- gophercise-7/cmd/main.go
+++ gophercise-7/cmd/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"encoding/binary"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/boltdb/bolt"
+	"github.com/dancing-koala/gophercises-impl/gophercise-7/pkg/task"
 	"os"
-	"strconv"
 	"strings"
 )
 
-var (
-	db         *bolt.DB
-	bucketName = []byte("TODO")
-)
-
-type Task struct {
-	Id   uint64
-	Name string
-	Done bool
-}
+var repo *task.TaskRepository
 
 func main() {
 	if len(os.Args) < 2 {
@@ -28,10 +17,10 @@ func main() {
 		return
 	}
 
-	var err error
-
-	db, err = bolt.Open("./db/gophercise_7.db", 0600, nil)
+	db, err := bolt.Open("./db/gophercise_7.db", 0600, nil)
 	defer db.Close()
+
+	repo = task.NewRepository(db)
 
 	handleErr(err)
 
@@ -79,130 +68,33 @@ Use "task [command] --help" for more information about a command.
 `)
 }
 
-func itob(val uint64) []byte {
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, val)
-	return b
-}
-
 func listTasks() {
-	dbErr := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(bucketName)
+	tasks, err := repo.ListNotDoneTasks()
 
-		if b == nil {
-			fmt.Println("No task in your list.")
-			return errors.New("Bucket does not exist!")
-		}
+	handleErr(err)
 
-		fmt.Println("You have the following tasks:")
+	if len(tasks) == 0 {
+		fmt.Println("You have no task in your list.")
+		return
+	}
 
-		c := b.Cursor()
-
-		task := &Task{}
-
-		for key, val := c.First(); key != nil; key, val = c.Next() {
-
-			json.Unmarshal(val, &task)
-
-			if task.Done {
-				continue
-			}
-
-			fmt.Printf("%d. %s\n", task.Id, task.Name)
-		}
-
-		return nil
-	})
-
-	handleErr(dbErr)
+	for _, task := range tasks {
+		fmt.Printf("%d. %s\n", task.Id, task.Name)
+	}
 }
 
 func addTask(taskName string) {
-	dbErr := db.Update(func(tx *bolt.Tx) error {
+	err := repo.AddTask(taskName)
 
-		b := tx.Bucket(bucketName)
-
-		var err error
-
-		if b == nil {
-			b, err = tx.CreateBucket(bucketName)
-
-			if err != nil {
-				return err
-			}
-		}
-
-		id, _ := b.NextSequence()
-
-		task := &Task{
-			Id:   id,
-			Name: taskName,
-			Done: false,
-		}
-
-		buf, err := json.Marshal(task)
-
-		if err != nil {
-			return err
-		}
-
-		b.Put(itob(id), buf)
-
-		return nil
-	})
-
-	handleErr(dbErr)
+	handleErr(err)
 
 	fmt.Printf("Added %q to your task list.\n", taskName)
 }
 
 func doTask(taskId string) {
-	dbErr := db.Update(func(tx *bolt.Tx) error {
+	task, err := repo.DoTask(taskId)
 
-		b := tx.Bucket(bucketName)
+	handleErr(err)
 
-		if b == nil {
-			fmt.Println("Task not found")
-			return errors.New("Bucket does not exist!")
-		}
-
-		id, err := strconv.ParseUint(taskId, 10, 64)
-
-		if err != nil {
-			return err
-		}
-
-		key := itob(id)
-		val := b.Get(key)
-
-		if val == nil {
-			fmt.Printf("Task <%s> does not exists!\n", taskId)
-			return errors.New("Task not found: " + taskId)
-		}
-
-		task := &Task{}
-
-		json.Unmarshal(val, &task)
-
-		if task.Done {
-			fmt.Printf("Task %q already done!\n", task.Name)
-			return errors.New("Task already done")
-		}
-
-		task.Done = true
-
-		buf, err := json.Marshal(task)
-
-		if err != nil {
-			return err
-		}
-
-		b.Put(key, buf)
-
-		fmt.Printf("You have completed the %q task.\n", task.Name)
-
-		return nil
-	})
-
-	handleErr(dbErr)
+	fmt.Printf("You have completed the %q task.\n", task.Name)
 }
